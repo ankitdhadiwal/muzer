b5488202afcd35490df78a444f2539ae8a7f0605
Ankit Dhadiwal <103865323+ankitdhadiwal@users.noreply.github.com>
2025-03-22 23:10:15 +0530

    Adding prisma schema

---
+++ app/app/api/streams/downvote/route.ts
@@ -0,0 +1,30 @@
+import {prismaClient} from "@/app/lib/db";
+import { getServerSession} from "next-auth";
+import {NextRequest, NextResponse} from "next/server";
+import {z} from "zod";
+
+const UpvoteSchema = z.object({
+    streamId: z.string(),
+})
+
+export async function POST(req: NextRequest) {
+    const session = await getServerSession();
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: session?.user?.email ?? ""
+        }
+    });
+
+    if(!user) {
+        return NextResponse.json({
+            message: "Unauthenticated"
+        }, {
+            status: 403
+        })
+    }
+
+
+
+    const data = UpvoteSchema.parse(await req.json());
+}
---
+++ app/app/page.tsx
@@ -0,0 +1,14 @@
+import { Appbar } from "./components/Appbar";
+
+export default function Home() {
+  return (
+    <div className="">
+      return (
+        <main>
+          <Appbar />
+        </main>
+      )
+      
+    </div>
+  );
+}
